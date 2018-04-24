b46fd0ab05b57a1fbadb215a8f4b8a17d33be8ac
markcam1 <32405690+markcam1@users.noreply.github.com>
2018-04-24 14:03:35 -0700

    add carousel js

--- assets/javascript/mc.js
+++ assets/javascript/mc.js
@@ -1,6 +1,6 @@
 var portfolio = {
     Hangman: {
-        title : "Hangman Word game", 
+        title : "Hangman Word Game", 
         sub: "JavaScript & jQuery",
         description: "This is the first game I created using HTML/CSS and JavaScript and jQuery." +
         "The code is far from perfect but I’m proud of the design theme and use of an object to hold the game parameters.",
@@ -45,7 +45,7 @@ var portfolio = {
         title: "Calendar", 
         sub: "??",
         description: "Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.",
-        imageUrl: "#",
+        imageUrl: "assets/images/calendar.jpg",
         deployUrl: "#",
         codeUrl: "https://calendarapp1bootcamp.github.io/calendar_final/",
     },
@@ -55,25 +55,32 @@ var portfolio = {
 var aClass = "list-group-item list-group-item-action flex-column align-items-start";
 var flexClass = "d-flex w-100 justify-content-between";
 var bootMarg = "mb-1";
-//<img src="..." class="rounded float-right" alt="...">
-
+var classImgThumb = "img-thumbnail rounded float-right w-25";
+var classCarouselItem = "carousel-item";
+var classCarousel_Image = "d-block w-100 carouselimage";
 
 $(document).ready(function(){
+
+
+
+
     function buttonPusher (){
-        console.log(portfolio)
+
         
         for (var key in portfolio){
             buttonData = portfolio[key];
-            screenRender(buttonData);
+            screenListRender(buttonData);
+            screenCarouselRender(buttonData);
         }
+
+
     } 
     
-    buttonPusher();
+
     
-    function screenRender(inButtons){
+    function screenListRender(inButtons){
     
-       newBtn = inButtons;
-       console.log("newBtn")
+       var newBtn = inButtons;
        var bannerTitle = newBtn.title;
        var bannerSubTitle = newBtn.sub;
        var bannerDesc = newBtn.description;
@@ -90,7 +97,8 @@ $(document).ready(function(){
        var headDivHeading = $("<h4>");
        var paragraphText = $("<p>");
        var smallText = $("<small class='text-muted'>").text(bannerSubTitle);
-       
+       var imgThumb = $("<img>");
+
        headDiv.attr({
            class: flexClass,
        })
@@ -100,27 +108,82 @@ $(document).ready(function(){
        paragraphText.attr({
            class: bootMarg,
        })
+       imgThumb.attr({
+           src: bannerImage,
+           class: classImgThumb,
+           alt: bannerTitle,
+       })
 
        $(headDivHeading).text(bannerTitle);
        $(paragraphText).text(bannerDesc);
 
-
        $(grandParentDiv).append(headDiv);
        $(headDiv).append(headDivHeading);
+
+       $(grandParentDiv).append(imgThumb);
        $(grandParentDiv).append(paragraphText);
        $(grandParentDiv).append(smallText);
 
-    
        $("#buttondrop").append(grandParentDiv)
-    
     }
 
 
+    function screenCarouselRender(inCarouselBtns){
+        
+        var newBtn = inCarouselBtns;
+        var bannerTitle = newBtn.title;
+        var bannerImage = newBtn.imageUrl;
+       
+       var carouselParentDiv = $("<div/>");
+       var carouselImage = $("<img/>");
+    
+       carouselParentDiv.attr({
+           class: classCarouselItem,
+       })
 
+       carouselImage.attr({
+        src: bannerImage,
+        class: classCarousel_Image,
+        alt: bannerTitle,
+    })
+       
+        $(carouselParentDiv).append(carouselImage);
 
+       $("#carouseldrop").append(carouselParentDiv)
+    
+    }
 
+    carouselDataMaker();
+    buttonPusher();
 
 })
+
+function carouselDataMaker (){
+
+    carouselCounter = Object.keys(portfolio);
+    portfoliioKeysArray = carouselCounter.length;
+
+    for (i = 1; i < portfoliioKeysArray; i++){
+
+        var slideNum = portfoliioKeysArray[i];
+
+        var carouselTarget = $("<li>");
+
+        carouselTarget.attr({
+            "data-target": "#carouselPortfolio",
+            "data-slide-to" : slideNum
+        })
+
+        $("#carouseldatadrop").append(carouselTarget);
+
+        //<li data-target="#carouselPortfolio" data-slide-to="1">
+
+    }
+
+
+}
+
+
 
 
 
